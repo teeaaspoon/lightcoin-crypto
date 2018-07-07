362337ff334c5d10431d559e8a02e0f1f7e3461f
teeaaspoon <tommypoon36@gmail.com>
2018-07-07 01:31:39 +0000

    everything working

--- index.js
+++ index.js
@@ -4,8 +4,19 @@ class Transaction {
     this.account = account;
   }
   commit() {
-    this.time = new Date();
-    this.account.addTransaction(this);
+    if (this.isAllowed() === true) {
+      this.time = new Date();
+      this.account.addTransaction(this);
+      return true;
+    }
+    console.log(`Insufficient funds on withdrawal for: ${this.amount}`);
+    return false;
+  }
+  isAllowed() {
+    if (this.constructor.name === "Withdrawal" && this.account.balance < this.amount) {
+      return false;
+    }
+    return true;
   }
 }
 
@@ -45,15 +56,41 @@ class Account {
 // DRIVER CODE BELOW
 // We use the code below to "drive" the application logic above and make sure it's working as expected
 
-const myAccount = new Account("snow-patrol");
+// const myAccount = new Account("snow-patrol");
 
 // console.log("Starting balance:", myAccount.balance);
 
-const t1 = new Deposit(120, myAccount);
+// const t1 = new Deposit(120, myAccount);
+// t1.commit();
 
-t1.commit();
+// const t2 = new Withdrawal(50, myAccount);
+// t2.commit();
 
-const t2 = new Withdrawal(50, myAccount);
-t2.commit();
+// const t3 = new Withdrawal(90, myAccount);
+// t3.commit();
 
 // console.log(`Ending Balance: ${myAccount.balance}`);
+
+// LIGHTHOUSE DRIVER CODE
+const myAccount = new Account();
+
+console.log("Starting Account Balance: ", myAccount.balance);
+
+console.log("Attempting to withdraw even $1 should fail...");
+const t1 = new Withdrawal(1.0, myAccount);
+console.log("Commit result:", t1.commit());
+console.log("Account Balance: ", myAccount.balance);
+
+console.log("Depositing should succeed...");
+const t2 = new Deposit(9.99, myAccount);
+console.log("Commit result:", t2.commit());
+console.log("Account Balance: ", myAccount.balance);
+
+console.log("Withdrawal for 9.99 should be allowed...");
+const t3 = new Withdrawal(9.99, myAccount);
+console.log("Commit result:", t3.commit());
+
+console.log("Ending Account Balance: ", myAccount.balance);
+console.log("Lookings like I'm broke again");
+
+console.log("Account Transaction History: ", myAccount.transactions);
