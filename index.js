1b89c828326a059a457bf5d95eda093f7436dcc4
teeaaspoon <tommypoon36@gmail.com>
2018-07-07 01:16:17 +0000

    added get balance method

--- index.js
+++ index.js
@@ -1,29 +1,59 @@
-let balance = 500.00;
-
-class Withdrawal {
-
-  constructor(amount) {
+class Transaction {
+  constructor(amount, account) {
     this.amount = amount;
+    this.account = account;
   }
-
   commit() {
-    balance -= this.amount;
+    this.time = new Date();
+    this.account.addTransaction(this);
   }
-
 }
 
+class Withdrawal extends Transaction {
+  get value() {
+    return -this.amount;
+  }
+}
 
+class Deposit extends Transaction {
+  get value() {
+    return this.amount;
+  }
+}
 
-
+class Account {
+  constructor(username) {
+    this.username = username;
+    this.transactions = [];
+  }
+  get balance() {
+    // Calculate the balance using the transaction objects
+    let balance = 0;
+    this.transactions.forEach(transaction => {
+      if (transaction.constructor.name === "Deposit") {
+        balance += transaction.amount;
+      } else {
+        balance -= transaction.amount;
+      }
+    });
+    return balance;
+  }
+  addTransaction(transaction) {
+    this.transactions.push(transaction);
+  }
+}
 // DRIVER CODE BELOW
 // We use the code below to "drive" the application logic above and make sure it's working as expected
 
-t1 = new Withdrawal(50.25);
+const myAccount = new Account("snow-patrol");
+
+// console.log("Starting balance:", myAccount.balance);
+
+const t1 = new Deposit(120, myAccount);
+
 t1.commit();
-console.log('Transaction 1:', t1);
 
-t2 = new Withdrawal(9.99);
+const t2 = new Withdrawal(50, myAccount);
 t2.commit();
-console.log('Transaction 2:', t2);
 
-console.log('Balance:', balance);
+// console.log(`Ending Balance: ${myAccount.balance}`);
